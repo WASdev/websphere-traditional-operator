fc15ef61a91f5c4f8fdb413602048cfd66e8de48
root <root@openings1.fyre.ibm.com>
2022-01-13 21:26:41 -0800

    Converting openliberty code for use

--- controllers/webspheretraditionalapplication_controller.go
+++ controllers/webspheretraditionalapplication_controller.go
@@ -18,19 +18,48 @@ package controllers
 
 import (
 	"context"
+	//	"fmt"
+	//	"os"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	//	networkingv1 "k8s.io/api/networking/v1"
+
+	"github.com/application-stacks/runtime-component-operator/common"
+	"github.com/go-logr/logr"
+
+	lutils "github.com/WASdev/websphere-traditional-operator/utils"
+	oputils "github.com/application-stacks/runtime-component-operator/utils"
+
+	webspheretraditionalv1alpha1 "github.com/WASdev/websphere-traditional-operator/api/v1alpha1"
+
+	//	prometheusv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	imagev1 "github.com/openshift/api/image/v1"
+	//	routev1 "github.com/openshift/api/route/v1"
+	imageutil "github.com/openshift/library-go/pkg/image/imageutil"
+	//	"github.com/pkg/errors"
+	//	appsv1 "k8s.io/api/apps/v1"
+	//	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	//	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	appswebspheretraditionaliov1alpha1 "github.com/WASdev/websphere-traditional-operator/api/v1alpha1"
+	//	"sigs.k8s.io/controller-runtime/pkg/builder"
+	//	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	//	"sigs.k8s.io/controller-runtime/pkg/event"
+	//	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	//	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // WebsphereTraditionalApplicationReconciler reconciles a WebsphereTraditionalApplication object
-type WebsphereTraditionalApplicationReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+type ReconcileWebsphereTraditional struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	oputils.ReconcilerBase
+	Log             logr.Logger
+	watchNamespaces []string
 }
 
 //+kubebuilder:rbac:groups=apps.webspheretraditional.io.ibm,resources=webspheretraditionalapplications,verbs=get;list;watch;create;update;patch;delete
@@ -46,17 +75,127 @@ type WebsphereTraditionalApplicationReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
-func (r *WebsphereTraditionalApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *ReconcileWebsphereTraditional) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
+	reqLogger := r.Log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconcile WebsphereTraditionalApplication - starting")
+	ns, err := oputils.GetOperatorNamespace()
+	// When running the operator locally, `ns` will be empty string
+	if ns == "" {
+		// Since this method can be called directly from unit test, populate `watchNamespaces`.
+		if r.watchNamespaces == nil {
+			r.watchNamespaces, err = oputils.GetWatchNamespaces()
+			if err != nil {
+				reqLogger.Error(err, "Error getting watch namespace")
+				return reconcile.Result{}, err
+			}
+		}
+		// If the operator is running locally, use the first namespace in the `watchNamespaces`
+		// `watchNamespaces` must have at least one item
+		ns = r.watchNamespaces[0]
+	}
 
-	// your logic here
+	configMap, err := r.GetOpConfigMap("websphere-traditional-operator", ns)
+	if err != nil {
+		reqLogger.Info("Failed to find websphere-traditional-operator config map")
+		common.Config = common.DefaultOpConfig()
+		configMap = &corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "websphere-traditional-operator", Namespace: ns}}
+		configMap.Data = common.Config
+	} else {
+		common.Config.LoadFromConfigMap(configMap)
+	}
 
+	_, err = controllerutil.CreateOrUpdate(context.TODO(), r.GetClient(), configMap, func() error {
+		configMap.Data = common.Config
+		return nil
+	})
+
+	if err != nil {
+		reqLogger.Info("Failed to update websphere-traditional-operator config map")
+	}
+
+	// Fetch the WebsphereTraditional instance
+	instance := &webspheretraditionalv1alpha1.WebsphereTraditionalApplication{}
+	var ba common.BaseComponent = instance
+	err = r.GetClient().Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if kerrors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	// Check if the WebsphereTraditionalApplication instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	isInstanceMarkedToBeDeleted := instance.GetDeletionTimestamp() != nil
+	if isInstanceMarkedToBeDeleted {
+		if lutils.Contains(instance.GetFinalizers(), applicationFinalizer) {
+			// Run finalization logic for applicationFinalizer. If the finalization logic fails, don't remove the
+			// finalizer so that we can retry during the next reconciliation.
+			if err := r.finalizeWebsphereTraditionalApplication(reqLogger, instance, instance.Name+"-serviceability", instance.Namespace); err != nil {
+				return reconcile.Result{}, err
+			}
+
+			// Remove applicationFinalizer. Once all finalizers have been removed, the object will be deleted.
+			instance.SetFinalizers(lutils.Remove(instance.GetFinalizers(), applicationFinalizer))
+			err := r.GetClient().Update(context.TODO(), instance)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+		}
+		return reconcile.Result{}, nil
+	}
+
+	//Brad Continue here
+	imageReferenceOld := instance.Status.ImageReference
+	instance.Status.ImageReference = instance.Spec.ApplicationImage
+	if r.IsOpenShift() {
+		image, err := imageutil.ParseDockerImageReference(instance.Spec.ApplicationImage)
+		if err == nil {
+			isTag := &imagev1.ImageStreamTag{}
+			isTagName := imageutil.JoinImageStreamTag(image.Name, image.Tag)
+			isTagNamespace := image.Namespace
+			if isTagNamespace == "" {
+				isTagNamespace = instance.Namespace
+			}
+			key := types.NamespacedName{Name: isTagName, Namespace: isTagNamespace}
+			err = r.GetAPIReader().Get(context.Background(), key, isTag)
+			// Call ManageError only if the error type is not found or is not forbidden. Forbidden could happen
+			// when the operator tries to call GET for ImageStreamTags on a namespace that doesn't exists (e.g.
+			// cannot get imagestreamtags.image.openshift.io in the namespace "navidsh": no RBAC policy matched)
+			if err == nil {
+				image := isTag.Image
+				if image.DockerImageReference != "" {
+					instance.Status.ImageReference = image.DockerImageReference
+				}
+			} else if err != nil && !kerrors.IsNotFound(err) && !kerrors.IsForbidden(err) {
+				return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
+			}
+		}
+	}
+
+	reqLogger.Info("Brad test text")
+	if imageReferenceOld != instance.Status.ImageReference {
+		reqLogger.Info("Updating status.imageReference", "status.imageReference", instance.Status.ImageReference)
+		err = r.UpdateStatus(instance)
+		if err != nil {
+			reqLogger.Error(err, "Error updating OpenLiberty status")
+			return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
+		}
+	}
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *WebsphereTraditionalApplicationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *ReconcileWebsphereTraditional) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appswebspheretraditionaliov1alpha1.WebsphereTraditionalApplication{}).
+		For(&webspheretraditionalv1alpha1.WebsphereTraditionalApplication{}).
 		Complete(r)
+}
+
+func (r *ReconcileWebsphereTraditional) finalizeWebsphereTraditionalApplication(reqLogger logr.Logger, olapp *webspheretraditionalv1alpha1.WebsphereTraditionalApplication, pvcName string, pvcNamespace string) error {
+	r.deletePVC(reqLogger, pvcName, pvcNamespace)
+	return nil
 }
---
+++ utils/utils.go
@@ -0,0 +1,77 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	webspheretraditionalv1alpha1 "github.com/WASdev/websphere-traditional-operator/api/v1alpha1"
+	rcoutils "github.com/application-stacks/runtime-component-operator/utils"
+	routev1 "github.com/openshift/api/route/v1"
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// Utility methods specific to Open Liberty and its configuration
+
+var log = logf.Log.WithName("webspheretraditional_utils")
+
+func requiredFieldMessage(fieldPaths ...string) string {
+	return "must set the field(s): " + strings.Join(fieldPaths, ",")
+}
+
+func findEnvVar(name string, envList []corev1.EnvVar) (*corev1.EnvVar, bool) {
+	for i, val := range envList {
+		if val.Name == name {
+			return &envList[i], true
+		}
+	}
+	return nil, false
+}
+
+func normalizeEnvVariableName(name string) string {
+	return strings.NewReplacer("-", "_", ".", "_").Replace(strings.ToUpper(name))
+}
+
+// getValue returns value for string
+func getValue(v interface{}) string {
+	switch v.(type) {
+	case string:
+		return v.(string)
+	case bool:
+		return strconv.FormatBool(v.(bool))
+	default:
+		return ""
+	}
+}
+
+func Contains(list []string, s string) bool {
+	for _, v := range list {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func Remove(list []string, s string) []string {
+	for i, v := range list {
+		if v == s {
+			list = append(list[:i], list[i+1:]...)
+		}
+	}
+	return list
+}
